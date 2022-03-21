9c484531176588edbcbfbae6c71d7ac56363fb1b
alaeddinenjima777 <94965193+alaeddinenjima777@users.noreply.github.com>
2022-03-21 10:27:25 -0700

    Add files via upload

---
+++ Address.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Address () {
+ return <h1> Rue De Bizerte Chenini Nahal </h1>
+}
+
+export default Address ;
---
+++ FullName.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Fullname () {
+ return <h1> ALA Eddine Njima</h1>
+}
+
+
+export default Fullname ;
